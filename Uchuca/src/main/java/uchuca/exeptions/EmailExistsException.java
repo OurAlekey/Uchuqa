edd0b41e660d263a394809cc2522ca57e10c1f98
OurAlekey <sayalex35@gmail.com>
2023-09-13 10:03:35 -0600

    SE AGREGO EL JSON PARA LA EXEPCION DE ERRORES

---
+++ Uchuca/src/main/java/uchuca/exeptions/EmailExistsException.java
@@ -0,0 +1,9 @@
+package uchuca.exeptions;
+
+public class EmailExistsException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public EmailExistsException(String message) {
+        super(message);
+    }
+}
